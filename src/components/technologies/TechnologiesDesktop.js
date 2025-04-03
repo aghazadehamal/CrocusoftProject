a6c4077d8a07feb2313585df0e77b1e260e83391
aghazadehamal@gmail.com <aghazadehamal@gmail.com>
2025-04-03 15:18:53 +0400

    I changed next and prev icons

--- src/components/technologies/TechnologiesDesktop.js
+++ src/components/technologies/TechnologiesDesktop.js
@@ -18,6 +18,8 @@ export default function TechnologiesDesktop() {
     }
   };
 
+  const isFirst = startIndex === 0;
+
   return (
     <div className="text-center py-10 pr-32 pl-32">
       <div className="pl-12">
@@ -34,8 +36,19 @@ export default function TechnologiesDesktop() {
       </div>
 
       <div className="mt-6 flex justify-center gap-4">
-        <button onClick={handlePrev} className="bg-purple-300 px-4 py-2 rounded-lg">⬅</button>
-        <button onClick={handleNext} className="bg-purple-600 text-white px-4 py-2 rounded-lg">➡</button>
+        <button
+          onClick={handlePrev}
+          className={`px-4 py-2 rounded-md ${isFirst ? "bg-gray-300 text-gray-600 cursor-not-allowed" : "bg-customPurple text-white"}`}
+          disabled={isFirst}
+        >
+          &lt;
+        </button>
+        <button
+          onClick={handleNext}
+          className="bg-customPurple text-white px-4 py-2 rounded-md"
+        >
+          &gt;
+        </button>
       </div>
     </div>
   );
